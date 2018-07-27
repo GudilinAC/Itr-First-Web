01c33d746fde6fa11dcccbe66cdbc82e8773d79f
Gudilin AC <GudilinAC@gmail.com>
2018-07-27 13:01:25 +0300

    First commit

---
+++ Web/Models/AppUserManager.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using System.ComponentModel.DataAnnotations;
+
+namespace Web.Models
+{
+    public class AppUserManager : UserManager<ApplicationUser>
+    {
+        public AppUserManager(IUserStore<ApplicationUser> store)
+            : base(store)
+        { }
+
+        public static AppUserManager Create(IdentityFactoryOptions<AppUserManager> options,
+            IOwinContext context)
+        {
+            ApplicationDbContext db = context.Get<ApplicationDbContext>();
+            AppUserManager manager = new AppUserManager(new UserStore<ApplicationUser>(db));
+            return manager;
+        }
+
+    }
+
+    public class CreateModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
